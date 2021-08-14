43fd6ac0a99863da512df9bc0ff60d2932b6dd94
lekwan <88898884+lekwan@users.noreply.github.com>
2021-08-14 08:44:37 +0700

    Add files via upload

---
+++ script/page.js
@@ -0,0 +1,418 @@
+//=========================================
+// File name: page_NNNNN.js
+//-----------------------------------------
+// Project : QuizFaber 4.0.20
+// Licence : GNU General Public License v3.0
+// Author  : Luca Galli
+// Email   : info@quizfaber.com
+//-----------------------------------------
+// Script for NNNNN-th question
+//=========================================
+
+var allInitQuestionFunctions = new Array (
+    InitQuestion1,
+    InitQuestion2,
+    InitQuestion3,
+    InitQuestion4,
+    InitQuestion5,
+    InitQuestion6,
+    InitQuestion7,
+    InitQuestion8,
+    InitQuestion9,
+    InitQuestion10,
+);
+
+
+
+$(document).ready(function ()
+{
+	const questionIndex = 0;
+	const questionTime = 0;
+
+	PageLoad(questionIndex, questionTime);
+
+	
+});
+
+$(window).on("beforeunload", function ()
+{
+	PageUnload();
+});
+
+$(window).on("scroll", function ()
+{
+	PageOnScroll();
+});
+
+
+/* Code generated function */
+function InitQuestion1()
+{
+    const type = QMAKE_BOOLEAN;
+    const questionIndex = 0;
+    var choice, valuation, noChoice = false;;
+    var weight = DecodeNumber('zwz+qlOfQPs=', 423, 0, 99999);
+    var numOfAnswers = 2;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.shortTextQuestion = "Cocokkan kolom sebelah kiri dengan kolom sebelah kanan !  Perhatikan Gambar stiker milik Anton berikut ini !     Pilihlah \"Benar\" atau \"Salah\" Pernyataan berikut ini !";
+    choice = GetBooleanChoice(questionIndex,0);
+    noChoice = noChoice || !IsBooleanChoiceSelected(questionIndex, 0);
+    valuation = DecodeNumber('iWJ+GZnYhos=', 423, -1, 1);
+    if (valuation === -2) PrintWrongKeyword(questionIndex);
+    var answer0 = new Answer(choice, valuation, 1, 0, '');
+    answer0.shortTextAnswer = "Stiker Anton yang paling sedikit adalah stiker berbentuk persegi";
+    question.answers.push(answer0);
+    choice = GetBooleanChoice(questionIndex,1);
+    noChoice = noChoice || !IsBooleanChoiceSelected(questionIndex, 1);
+    valuation = DecodeNumber('zwz+qlOfQPs=', 425, -1, 1);
+    if (valuation === -2) PrintWrongKeyword(questionIndex);
+    var answer1 = new Answer(choice, valuation, 1, 0, '');
+    answer1.shortTextAnswer = "Stiker Anton yang paling banyak adalah stiker berbentuk lingkaran";
+    question.answers.push(answer1);
+    question.noChoice = noChoice;
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion2()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 1;
+    var weight = DecodeNumber('MOUJGwHYhBY=', 433, 0, 99999);
+    var numOfAnswers = 4;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = false;
+    question.shortTextQuestion = "Andi mendapatkan kue 5/6 bagian dan Susi mendapat 6/7 bagian dari kue yang sama.   Pilihlah semua pernyataan yang \"Benar\" dari pernyataan dibawah ini !";
+    question.timeToAnswer = 0;
+    question.noChoice = true;
+    var valuation1 = DecodeNumber('MOUJGwHYhBY=', 435, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var choice1 = GetMultipleAnswerChoice(questionIndex, 0);
+    if (choice1 === 1) question.noChoice = false;
+    var answer1 = new Answer(choice1, valuation1, 1, 0, '');
+    answer1.shortTextAnswer = "Andi mendapatkan bagian kue yang lebih banyak";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('wjzOJnnCVqs=', 437, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var choice2 = GetMultipleAnswerChoice(questionIndex, 1);
+    if (choice2 === 1) question.noChoice = false;
+    var answer2 = new Answer(choice2, valuation2, 1, 0, '');
+    answer2.shortTextAnswer = "Susi mendapatkan bagian kue lebih banyak";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('Y+a4gtkCc/c=', 276, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var choice3 = GetMultipleAnswerChoice(questionIndex, 2);
+    if (choice3 === 1) question.noChoice = false;
+    var answer3 = new Answer(choice3, valuation3, 1, 0, '');
+    answer3.shortTextAnswer = "Andi mendapatkan bagian kue lebih sedikit";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('Y+a4gtkCc/c=', 278, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var choice4 = GetMultipleAnswerChoice(questionIndex, 3);
+    if (choice4 === 1) question.noChoice = false;
+    var answer4 = new Answer(choice4, valuation4, 1, 0, '');
+    answer4.shortTextAnswer = "Susi mendapatkan bagian kue yang lebih banyak";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion3()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 2;
+    var weight = DecodeNumber('vGaiGo0FvhU=', 942, 0, 99999);
+    var numOfAnswers = 5;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = false;
+    question.shortTextQuestion = "Ilustrasi     Diketahui banyak rumah seluruhnya pada gambar di atas adalah 9 rumah. Penomorannya secara berutuan menggunakan angka ganjil dimulai dari 1 (satu) untuk nomor rumah yang berada paling kanan.  Nomor rumah pada urutan keenam dari kanan dan nomor rumah pada urutan ketujuh dari kiri adalah ... ";
+    question.timeToAnswer = 0;
+    question.noChoice = true;
+    var valuation1 = DecodeNumber('WQJ+f9clT+w=', 944, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var choice1 = GetMultipleAnswerChoice(questionIndex, 0);
+    if (choice1 === 1) question.noChoice = false;
+    var answer1 = new Answer(choice1, valuation1, 1, 0, '');
+    answer1.shortTextAnswer = "5";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('WQJ+f9clT+w=', 946, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var choice2 = GetMultipleAnswerChoice(questionIndex, 1);
+    if (choice2 === 1) question.noChoice = false;
+    var answer2 = new Answer(choice2, valuation2, 1, 0, '');
+    answer2.shortTextAnswer = "7";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('Vj7e30UG0HM=', 948, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var choice3 = GetMultipleAnswerChoice(questionIndex, 2);
+    if (choice3 === 1) question.noChoice = false;
+    var answer3 = new Answer(choice3, valuation3, 1, 0, '');
+    answer3.shortTextAnswer = "9";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('x2HuXFqmvQc=', 950, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var choice4 = GetMultipleAnswerChoice(questionIndex, 3);
+    if (choice4 === 1) question.noChoice = false;
+    var answer4 = new Answer(choice4, valuation4, 1, 0, '');
+    answer4.shortTextAnswer = "11";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    var valuation5 = DecodeNumber('f9RR8yG0BDQ=', 788, -1, 1);
+    if (valuation5 === -2) PrintWrongKeyword(questionIndex);
+    var choice5 = GetMultipleAnswerChoice(questionIndex, 4);
+    if (choice5 === 1) question.noChoice = false;
+    var answer5 = new Answer(choice5, valuation5, 1, 0, '');
+    answer5.shortTextAnswer = "13";
+    answer5.shortTextRemark = "";
+    question.answers.push(answer5);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion4()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 3;
+    var weight = DecodeNumber('fmXjT+gWaUg=', 788, 0, 99999);
+    var numOfAnswers = 4;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = false;
+    question.shortTextQuestion = "Menurut pola bilangan Fibonacci  1, 2, 3, 5, 8, 13, ... dua bilangan selanjutnya yang benar adalah ...";
+    question.timeToAnswer = 0;
+    question.noChoice = true;
+    var valuation1 = DecodeNumber('fmXjT+gWaUg=', 790, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var choice1 = GetMultipleAnswerChoice(questionIndex, 0);
+    if (choice1 === 1) question.noChoice = false;
+    var answer1 = new Answer(choice1, valuation1, 1, 0, '');
+    answer1.shortTextAnswer = "20";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('IDeG/5TVj2g=', 792, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var choice2 = GetMultipleAnswerChoice(questionIndex, 1);
+    if (choice2 === 1) question.noChoice = false;
+    var answer2 = new Answer(choice2, valuation2, 1, 0, '');
+    answer2.shortTextAnswer = "21";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('IDeG/5TVj2g=', 794, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var choice3 = GetMultipleAnswerChoice(questionIndex, 2);
+    if (choice3 === 1) question.noChoice = false;
+    var answer3 = new Answer(choice3, valuation3, 1, 0, '');
+    answer3.shortTextAnswer = "27";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('h9ZGPKrPBDY=', 796, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var choice4 = GetMultipleAnswerChoice(questionIndex, 3);
+    if (choice4 === 1) question.noChoice = false;
+    var answer4 = new Answer(choice4, valuation4, 1, 0, '');
+    answer4.shortTextAnswer = "34";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion5()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 4;
+    var weight = DecodeNumber('NHOKpcZbX0g=', 516, 0, 99999);
+    var numOfAnswers = 4;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = true;
+    question.shortTextQuestion = "Foto berikut adalah tempat alat tulis milik Raisa yang berada di meja belajarnya.       Jika tempat alat tulis tersebut difoto dari atas, manakah gambar yang sesuai dengan tempat alat tulis tersebut?";
+    question.timeToAnswer = 0;
+    var choice = GetSingleAnswerChoice(questionIndex);
+    if (choice === 0) question.noChoice = true;
+    var valuation1 = DecodeNumber('NHOKpcZbX0g=', 518, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var answer1 = new Answer((choice == 1 ? 1 : 0), valuation1, 1, 0, '');
+    answer1.shortTextAnswer = " ";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('9vztBBR8oQQ=', 520, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var answer2 = new Answer((choice == 2 ? 1 : 0), valuation2, 1, 0, '');
+    answer2.shortTextAnswer = " ";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('xar6jq/HmQ0=', 522, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var answer3 = new Answer((choice == 3 ? 1 : 0), valuation3, 1, 0, '');
+    answer3.shortTextAnswer = " ";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('crh+e4PyMyM=', 524, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var answer4 = new Answer((choice == 4 ? 1 : 0), valuation4, 1, 0, '');
+    answer4.shortTextAnswer = " ";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion6()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 5;
+    var weight = DecodeNumber('2dWB7rUGSQc=', 526, 0, 99999);
+    var numOfAnswers = 4;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = true;
+    question.shortTextQuestion = "Dalam suatu gedung pertunjukan terdapat 9 baris kursi. Pada baris pertama terdapat 7 kursi, baris kedua 12 kursi, baris ketiga 10 kursi, baris keempat 15 kursi, baris kelima ada 13 kursi, dan seterusnya mengikuti pola yang sama.  Banyaknya kursi pada baris terakhir adalah ...";
+    question.timeToAnswer = 0;
+    var choice = GetSingleAnswerChoice(questionIndex);
+    if (choice === 0) question.noChoice = true;
+    var valuation1 = DecodeNumber('FIosg/co1k0=', 364, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var answer1 = new Answer((choice == 1 ? 1 : 0), valuation1, 1, 0, '');
+    answer1.shortTextAnswer = "21";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('14hipsb286I=', 366, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var answer2 = new Answer((choice == 2 ? 1 : 0), valuation2, 1, 0, '');
+    answer2.shortTextAnswer = "19";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('14hipsb286I=', 368, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var answer3 = new Answer((choice == 3 ? 1 : 0), valuation3, 1, 0, '');
+    answer3.shortTextAnswer = "18";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('zEOUKFRzpQk=', 370, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var answer4 = new Answer((choice == 4 ? 1 : 0), valuation4, 1, 0, '');
+    answer4.shortTextAnswer = "16";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion7()
+{
+    const type = QMAKE_MULTIANS;
+    const questionIndex = 6;
+    var weight = DecodeNumber('77SU0glKeKI=', 372, 0, 99999);
+    var numOfAnswers = 4;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.isSingleAns = true;
+    question.shortTextQuestion = "Dalam suatu gedung pertunjukan terdapat 9 baris kursi. Pada baris pertama terdapat 7 kursi, baris kedua 12 kursi, baris ketiga 10 kursi, baris keempat 15 kursi, baris kelima ada 13 kursi, dan seterusnya mengikuti pola yang sama. Ternyata penonton lebih suka duduk di bangku pada urutan tiga baris terakhir, jumlah kursi yang dapat ditempati pada tiga baris tersebut adalah ...   ";
+    question.timeToAnswer = 0;
+    var choice = GetSingleAnswerChoice(questionIndex);
+    if (choice === 0) question.noChoice = true;
+    var valuation1 = DecodeNumber('77SU0glKeKI=', 374, -1, 1);
+    if (valuation1 === -2) PrintWrongKeyword(questionIndex);
+    var answer1 = new Answer((choice == 1 ? 1 : 0), valuation1, 1, 0, '');
+    answer1.shortTextAnswer = "51";
+    answer1.shortTextRemark = "";
+    question.answers.push(answer1);
+    var valuation2 = DecodeNumber('AXrM5gp2AB4=', 376, -1, 1);
+    if (valuation2 === -2) PrintWrongKeyword(questionIndex);
+    var answer2 = new Answer((choice == 2 ? 1 : 0), valuation2, 1, 0, '');
+    answer2.shortTextAnswer = "55";
+    answer2.shortTextRemark = "";
+    question.answers.push(answer2);
+    var valuation3 = DecodeNumber('zauwg5di/R0=', 378, -1, 1);
+    if (valuation3 === -2) PrintWrongKeyword(questionIndex);
+    var answer3 = new Answer((choice == 3 ? 1 : 0), valuation3, 1, 0, '');
+    answer3.shortTextAnswer = "56";
+    answer3.shortTextRemark = "";
+    question.answers.push(answer3);
+    var valuation4 = DecodeNumber('zauwg5di/R0=', 380, -1, 1);
+    if (valuation4 === -2) PrintWrongKeyword(questionIndex);
+    var answer4 = new Answer((choice == 4 ? 1 : 0), valuation4, 1, 0, '');
+    answer4.shortTextAnswer = "64";
+    answer4.shortTextRemark = "";
+    question.answers.push(answer4);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion8()
+{
+    const type = QMAKE_OPENANS;
+    const questionIndex = 7;
+    var weight = DecodeNumber('jtbpE4fhsOc=', 577, 0, 99999);
+    var numOfAnswers = 0;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.shortTextQuestion = "Aliyah menumpuk beberapa telur menjadi bentuk berikut ini,      Berapa banyaknya telur yang berada pada tingkat ketiga dari dasar tumpukan ? ";
+    var comment = $('#comment' + questionIndex).val();
+    var answer = new Answer(0,0,1,0,comment);
+    question.answers.push(answer);
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion9()
+{
+    const type = QMAKE_BOOLEAN;
+    const questionIndex = 8;
+    var choice, valuation, noChoice = false;;
+    var weight = DecodeNumber('9MT6yxSJakI=', 141, 0, 99999);
+    var numOfAnswers = 2;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.shortTextQuestion = "Perhatikan gambar berikut ini !     Aliyah kan menambahkan dua tingkat diabawah susunan tersebut, tentukan nilai kebenaran dua pernyataan dibawah ini !  ";
+    choice = GetBooleanChoice(questionIndex,0);
+    noChoice = noChoice || !IsBooleanChoiceSelected(questionIndex, 0);
+    valuation = DecodeNumber('9MT6yxSJakI=', 141, -1, 1);
+    if (valuation === -2) PrintWrongKeyword(questionIndex);
+    var answer0 = new Answer(choice, valuation, 1, 0, '');
+    answer0.shortTextAnswer = "Jumlah seluruh telur setelah ditambahkan tingkat yang baru adalah 204 butir ";
+    question.answers.push(answer0);
+    choice = GetBooleanChoice(questionIndex,1);
+    noChoice = noChoice || !IsBooleanChoiceSelected(questionIndex, 1);
+    valuation = DecodeNumber('wjWVH+h7DCs=', 143, -1, 1);
+    if (valuation === -2) PrintWrongKeyword(questionIndex);
+    var answer1 = new Answer(choice, valuation, 1, 0, '');
+    answer1.shortTextAnswer = "Selisih telur pada susunan semula dengan susunan yang baru adalah 113 butir";
+    question.answers.push(answer1);
+    question.noChoice = noChoice;
+    return question;
+
+}
+/* Code generated function */
+function InitQuestion10()
+{
+    const type = QMAKE_OPENANS;
+    const questionIndex = 9;
+    var weight = DecodeNumber('6BQAPRTZ97Y=', 151, 0, 99999);
+    var numOfAnswers = 0;
+    var question = new Question(type, weight, numOfAnswers);
+    question.answers.length = 0;
+    question.shortTextQuestion = "Dalam suatu gedung pertunjukan terdapat 9 baris kursi. Pada baris pertama terdapat 7 kursi, baris kedua 12 kursi, baris ketiga 10 kursi, baris keempat 15 kursi, baris kelima ada 13 kursi, dan seterusnya mengikuti pola yang sama. Pada suatu malam, semua kursi pada gedung tersebut terisi penuh. Apabila harga tiket pertunjukan untuk satu kursi adalah Rp50.000, berapakah uang yang terkumpul pada malam tersebut ?   ";
+    var comment = $('#comment' + questionIndex).val();
+    var answer = new Answer(0,0,1,0,comment);
+    question.answers.push(answer);
+    return question;
+
+}
+
+
+
+
+
+
